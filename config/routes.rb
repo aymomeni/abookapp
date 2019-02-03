f8d83bbcb8ccddb7c3125494b5fac4c1e3b7f2c2
Ali Momeni <aymomeni@gmail.com>
2019-02-02 17:59:47 -0700

    first controllers and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  #simple route
   get 'demo/index'
+  get 'demo/hello'
+
+  #default route may go away in future versions of rails
+  #get ':controller(/:action(/:id))'
+
+  #root route (overrides the generic ruby webpage with a specific action and controller)
+  root 'demo#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
