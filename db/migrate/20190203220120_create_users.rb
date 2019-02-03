af363aca9b890e8d38cab46a1a0d159b1b44132e
Ali Momeni <aymomeni@gmail.com>
2019-02-03 16:04:29 -0700

    more migrations

--- db/migrate/20190203220120_create_users.rb
+++ db/migrate/20190203220120_create_users.rb
@@ -2,12 +2,21 @@ class CreateUsers < ActiveRecord::Migration[5.2]
 
   def up
     create_table :users do |t|
+      # primary key is automatically added by rails to each table
+
       # where column definitions go
-      t.column "first_name", :string
-      t.string "last_name"
-      t.string "email", :default =>
-      t.column "address", :string
+      t.column "first_name", :string, :limit => 25
+      t.string "last_name", :limit => 50
+      t.string "email", :default => '', :null => false
+      t.string "password", :limit => 40
+      t.column "address", :string, :limit => 25
       t.column "phone_number", :integer
+
+
+      t.timestamps
+      #t.datetime "created_at"
+      #t.datetime "updated_at"
+
 
       #other datatypes
       #binary, boolean date, datetime, decimal, float
@@ -16,8 +25,6 @@ class CreateUsers < ActiveRecord::Migration[5.2]
       #column options
       # :limit => size, :default => value, :null => true or failse,
       # :precision => number, :scale => number
-
-      t.timestamps
     end
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_03_044210) do
+ActiveRecord::Schema.define(version: 2019_02_03_220120) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name", limit: 25
+    t.string "last_name", limit: 50
+    t.string "email", default: "", null: false
+    t.string "password", limit: 40
+    t.string "address", limit: 25
+    t.integer "phone_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
